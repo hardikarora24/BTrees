390230f40fe1e2cd4d9ac14827072766d6165e59
HARDIK <hardiksanjayarora@gmail.com>
2021-10-28 16:28:13 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+//Hardik Arora(2K20/SE/53)
+//Kartik Bansal(2K20/SE/68)
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int bool;
+
+#define true 1
+#define false 0
+
+typedef struct Node Node;
+struct Node {
+
+};
+
+typedef struct btree btree;
+struct btree {
+
+};
+
+int main() {
+  printf("Hardik Arora-2K20/SE/53\nKartik Bansal-2K20/SE/68");
+  return 0;
+}
